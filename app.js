44e93f41bb2fc182e1d6273265792a1eccd0b735
Mark A. <mark001616@gmail.com>
2024-10-07 21:09:47 -0400

    Add support for ejs views and html forms on post

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+const path = require("node:path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 3000;
 
 const indexRouter = require("./routes/indexRouter");
