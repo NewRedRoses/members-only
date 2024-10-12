09748a3d3763dc3954a3bec54fc398504d8974dc
Mark A. <mark001616@gmail.com>
2024-10-12 17:01:40 -0400

    Initialize route for clubs

---
+++ controllers/clubsController.js
@@ -0,0 +1,5 @@
+function clubsRouteGet(req, res, next) {
+  res.render("clubs");
+}
+
+module.exports = { clubsRouteGet };
--- routes/clubsRouter.js
+++ routes/clubsRouter.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
-
 const clubsRouter = Router();
 
-clubsRouter.get("", (req, res) => {});
+const { clubsRouteGet } = require("../controllers/clubsController");
+
+clubsRouter.get("", clubsRouteGet);
 
 module.exports = clubsRouter;
