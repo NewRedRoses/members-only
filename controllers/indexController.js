8009204a66e0448ff16544bd1aa11bd511c310c8
Mark A. <mark001616@gmail.com>
2024-10-13 16:03:05 -0400

    Add basic club styling & header color change

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,8 @@
+const db = require("../db/queries");
+
 async function indexRouteGet(req, res, next) {
-  res.render("index", { user: req.user });
+  const posts = await db.getPosts();
+  res.render("index", { user: req.user, posts: posts });
 }
 
 module.exports = { indexRouteGet };
