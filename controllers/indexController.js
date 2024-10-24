d6beee38dfd9163eb25c4e54f389100897bba153
Mark A. <mark001616@gmail.com>
2024-10-23 22:04:01 -0400

    Add validator for creating posts

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,10 +1,35 @@
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
+
+const postLengthErr = "is too long, please consider shortening it.";
+const validatePost = [
+  body("title")
+    .trim()
+    .optional()
+    .isLength({ max: 50 })
+    .withMessage(`The title ${postLengthErr}`),
+  body("message")
+    .notEmpty()
+    .withMessage("Message cannot be empty!")
+    .isLength({ max: 255 })
+    .withMessage(`The message ${postLengthErr}`),
+];
 
 async function indexRouteGet(req, res, next) {
   const posts = await db.getPosts();
   res.render("index", { user: req.user, posts: posts });
 }
 async function indexRoutePost(req, res, next) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).render("index", {
+      user: req.user,
+      posts: await db.getPosts(),
+      errors: errors.array(),
+    });
+  }
+
   if (req.user) {
     await db.createPost(
       req.body.message,
@@ -15,4 +40,7 @@ async function indexRoutePost(req, res, next) {
     res.redirect("/");
   }
 }
-module.exports = { indexRouteGet, indexRoutePost };
+module.exports = {
+  indexRouteGet,
+  indexRoutePost: [validatePost, indexRoutePost],
+};
