8586cabb5657d301251dfc4cc1a47e039b99aa5e
Mark A. <mark001616@gmail.com>
2024-10-28 22:28:51 -0400

    Change to db url instead

--- db/pool.js
+++ db/pool.js
@@ -3,9 +3,5 @@ require("dotenv").config();
 
 const { PGHOST, PGUSER, DBDATABASE, PGPASSWORD, PGPORT } = process.env;
 module.exports = new Pool({
-  host: PGHOST,
-  user: PGUSER,
-  database: DBDATABASE,
-  password: PGPASSWORD,
-  port: PGPORT,
+  connectionString: process.env.DATABASE_URL,
 });
