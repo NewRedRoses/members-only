573ccf07776ab3c4bbbc8c767a97f93f7cf0936f
Mark A. <mark001616@gmail.com>
2024-10-28 22:08:44 -0400

    Update name convention for pg db env variables

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD } = process.env;
+const { PGHOST, PGUSER, DBDATABASE, PGPASSWORD, PGPORT } = process.env;
 module.exports = new Pool({
-  host: DB_HOST,
-  user: DB_USER,
-  database: DB_NAME,
-  password: DB_PASSWORD,
-  port: 5432,
+  host: PGHOST,
+  user: PGUSER,
+  database: DBDATABASE,
+  password: PGPASSWORD,
+  port: PGPORT,
 });
